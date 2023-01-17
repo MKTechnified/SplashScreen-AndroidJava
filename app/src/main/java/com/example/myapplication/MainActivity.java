616e5ed9e26e02c265a79e8292e29e0e414644a9
MKTechnified <mktechnified@gmail.com>
2023-01-17 14:21:59 +0530

    removed toast

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,6 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Hello MK", Toast.LENGTH_SHORT).show();
     }
 }
